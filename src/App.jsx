098ab7eaf75f162e1d4456ee795780bd9109652e
KDragon <elkarouani@gmail.com>
2019-01-30 12:41:43 +0100

    Task: applying scss app file to the app js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "./sass/app.scss";
 
 class App extends Component {
   render() {
     return (
-      
+      <div className="app-container">
+      	  	<div className="main-container">
+      	  		Main Container
+      		</div>
+      </div>
     );
   }
 }
