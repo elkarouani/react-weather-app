ed3e743fed5c3c78341679e4fc6d1b404163570f
KDragon <elkarouani@gmail.com>
2019-02-02 13:25:48 +0100

    Task: applying react popper interactivity to the weather component

--- src/components/top/index.jsx
+++ src/components/top/index.jsx
@@ -1,19 +1,44 @@
 import React from "react";
 import "./style.scss";
 import Weather from "./weather";
+import { Manager, Reference, Popper } from 'react-popper';
 
 export default class TopSection extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			isSelectLocationOpen: false
+		};
+	}
+
+	onToggleSelectLocation() {
+		this.setState((prevState) => ({ isSelectLocationOpen: !prevState.isSelectLocationOpen }));
 	}
 
 	render() {
+		const { isSelectLocationOpen } = this.state;
 		return (
 			<div className="top-container">
 				<div className="title">Weather Up</div>
 				<Weather {...this.props} />
-				<button className="btn btn-select-location">Select Location</button>
+				
+				<Manager>
+				    <Reference>
+				      {({ ref }) => (
+				        <button className="btn btn-select-location" ref={ref} onClick={this.onToggleSelectLocation.bind(this)}>
+				        	Select Location
+				        </button>
+				      )}
+				    </Reference>
+				    <Popper placement="top">
+				      {({ ref, style, placement, arrowProps }) => ( isSelectLocationOpen && (
+				        <div ref={ref} style={style} data-placement={placement}>
+				          Popper element
+				          <div ref={arrowProps.ref} style={arrowProps.style} />
+				        </div>
+				      ))}
+				    </Popper>
+				</Manager>
 			</div>
 		);
 	}
